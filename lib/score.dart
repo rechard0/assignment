b1ce8b7583117d9ac793e9a77117d74f19703e84
Jang <rechardjang@naver.com>
2025-12-12 10:25:56 +0900

    ㄴㅁㅇㄹ

--- lib/score.dart
+++ lib/score.dart
@@ -1,13 +1,7 @@
 abstract class Score {
-  final String name;
   final int score;
 
-  const Score({required this.name, required this.score});
+  const Score({required this.score});
 
   void show() {}
-
-  @override
-  String toString() {
-    return 'StudentScore(name: $name, score: $score)';
-  }
 }
--- lib/student_score.dart
+++ lib/student_score.dart
@@ -1,10 +1,17 @@
 import 'package:dart_application_1/score.dart';
 
 class StudentScore extends Score {
-  StudentScore({required super.name, required super.score});
+  final String name;
+
+  StudentScore({required super.score, required this.name});
 
   @override
   void show() {
     super.show();
+  }
+
+  @override
+  String toString() {
+    return 'StudentScore(name: $name, score: ${super.score})';
   }
 }
